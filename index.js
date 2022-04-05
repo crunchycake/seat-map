2b734b27094c564f0908112c2ffc0f0148944496
Mateusz Cieślik <mateuszcieslik@gmail.com>
2022-04-05 22:59:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const oneSeat = document.querySelectorAll('.oneSeat');
+const seat = document.querySelectorAll('.seat');
+const active = document.querySelectorAll('.active');
+const label = document.querySelectorAll('label');
+const input = document.querySelectorAll('input');
+
+
+function freeSeateatMsg(e){
+    console.log(`kliknięto ${e.target.id}`);
+}
+
+
+
+oneSeat.forEach( seat => seat.addEventListener('click', freeSeateatMsg));
+
+
+
+for (let i = 0; i < oneSeat.length; i++) {
+    oneSeat[i].addEventListener('click', function(e){
+        if(e.target.classList.contains('active')){
+            e.target.classList.remove('active');
+            input[i].checked = false;
+            label[i].classList.remove('active');
+        } else {
+            e.target.classList.add('active');
+            input[i].checked = true;
+            label[i].classList.add('active');
+        }
+    });
+}
+
+
+
+
+
+
+
